2e48c75756f393c9fc970f2b0e7b207a1d8f815e
Pereyro <petrgubko@gmail.com>
2023-10-18 22:27:36 +0300

    External Private route element has been added and KekRouter has been modified by adding private route

--- src/shared/router/index.jsx
+++ src/shared/router/index.jsx
@@ -5,6 +5,7 @@ import Afisha from "../../pages/Afisha";
 import FilmPage from '../../pages/FlimPage';
 import BarGrill from '../../pages/BarGrill';
 import LoginPage from '../../pages/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 
 
@@ -12,9 +13,9 @@ function KekRouter() {
   return ( 
       <Routes>
         <Route path="/about" element={<About/> } />
-        <Route exact path="/afisha" element={<Afisha/> } />
-        <Route exact path="/afisha/:id" element={<FilmPage/> } />
-        <Route path="/bargrill" element={<BarGrill/>} />
+        <Route exact path="/afisha" element={<PrivateRoute element={<Afisha/>}/> } />
+        <Route exact path="/afisha/:id" element={<PrivateRoute element={<FilmPage/>} /> } />
+        <Route path="/bargrill" element={<PrivateRoute element={<BarGrill/>} />} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="*" element={<Navigate to="/about" />} />
       </Routes> 
